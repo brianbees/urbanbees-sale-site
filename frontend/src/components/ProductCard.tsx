ae7c65bca0050aa61c24a9824293337c4dda4f41
brianbees <brian@urbanbees.co.uk>
2026-02-12 10:23:07 +0000

    Show wishlist button text on all screen sizes

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -258,7 +258,7 @@ export default function ProductCard({ product, index }: ProductCardProps) {
                       d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
                     />
                   </svg>
-                  <span className="hidden md:inline">{inWishlist ? 'In Wishlist' : 'Wishlist'}</span>
+                  <span>{inWishlist ? 'In Wishlist' : 'Wishlist'}</span>
                 </button>
               </div>
             </div>
